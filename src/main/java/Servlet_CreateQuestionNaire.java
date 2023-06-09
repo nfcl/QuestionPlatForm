6a624b10837389c8bebf33ad405031feb9633b5c
nfcl <2872786663@qq.com>
2023-06-09 15:21:11 +0800

    正在用JSP标签重写所有的页面 重写好的页面: 1.CreateQuestionNaire 创建问卷页面 完成后端向数据库插入问卷的Servlet

--- src/main/java/Servlet_CreateQuestionNaire.java
+++ src/main/java/Servlet_CreateQuestionNaire.java
@@ -1,8 +1,15 @@
-import Sessions.QuestionNaire;
+import DruidUtil.DruidUtil;
 import Sessions.User;
-import jakarta.servlet.*;
-import jakarta.servlet.http.*;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.tomcat.jakartaee.commons.lang3.tuple.ImmutablePair;
+import org.apache.tomcat.jakartaee.commons.lang3.tuple.Pair;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.LinkedList;
 
 public class Servlet_CreateQuestionNaire extends HttpServlet {
@@ -18,10 +25,8 @@ public class Servlet_CreateQuestionNaire extends HttpServlet {
             int questionNum = 0;
 
             LinkedList<String> question_Enter = new LinkedList<>();
-            LinkedList<String> question_Single = new LinkedList<>();
-            LinkedList<String[]> question_Single_Option = new LinkedList<>();
-            LinkedList<String> question_Multiple = new LinkedList<>();
-            LinkedList<String[]> question_Multiple_Option = new LinkedList<>();
+            LinkedList<Pair<String, String[]>> question_Single = new LinkedList<>();
+            LinkedList<Pair<String, String[]>> question_Multiple = new LinkedList<>();
 
             try {
 
@@ -51,15 +56,23 @@ public class Servlet_CreateQuestionNaire extends HttpServlet {
                     }
                     case "Single": {
 
-                        question_Single.add(request.getParameter("QuestionNaire-Question-Title-" + i));
-                        question_Single_Option.add(request.getParameterValues("QuestionNaire-OptionCont-" + i));
+                        question_Single.add(
+                                new ImmutablePair<>(
+                                        request.getParameter("QuestionNaire-Question-Title-" + i),
+                                        request.getParameterValues("QuestionNaire-OptionCont-" + i)
+                                )
+                        );
 
                         break;
                     }
                     case "Multiple": {
 
-                        question_Multiple.add(request.getParameter("QuestionNaire-Question-Title-" + i));
-                        question_Multiple_Option.add(request.getParameterValues("QuestionNaire-OptionCont-" + i));
+                        question_Multiple.add(
+                                new ImmutablePair<>(
+                                        request.getParameter("QuestionNaire-Question-Title-" + i),
+                                        request.getParameterValues("QuestionNaire-OptionCont-" + i)
+                                )
+                        );
 
                         break;
                     }
@@ -67,6 +80,202 @@ public class Servlet_CreateQuestionNaire extends HttpServlet {
                 }
 
             }
+
+            Connection conn;
+
+            PreparedStatement stmt;
+
+            ResultSet rs;
+
+            conn = DruidUtil.getDataSource().getConnection();
+
+            stmt = conn.prepareStatement("" +
+                    "INSERT INTO questionnaire(" +
+                    "   questionnaire_Name," +
+                    "   user_Id, " +
+                    "   questionnaire_Starttime, " +
+                    "   questionnaire_Endtime, " +
+                    "   questionnaire_Limittime " +
+                    ") " +
+                    "VALUES(" +
+                    "   ?," +
+                    "   ?," +
+                    "   NOW()," +
+                    "   DATE_ADD(NOW(), interval 1 year)," +
+                    "   NULL" +
+                    ")"
+            );
+
+            stmt.setString(1, questionNaireTitle);
+            stmt.setString(2, questionNaireCreatorId);
+
+            stmt.executeUpdate();
+
+            stmt.close();
+
+            stmt = conn.prepareStatement("SELECT LAST_INSERT_ID()");
+
+            rs = stmt.executeQuery();
+
+            rs.next();
+
+            String questionNaireId = rs.getString(1);
+
+            rs.close();
+
+            stmt.close();
+
+            for (String enter : question_Enter) {
+
+                stmt = conn.prepareStatement("" +
+                        "INSERT INTO question(" +
+                        "   question_Cont, " +
+                        "   question_Kind, " +
+                        "   questionnaire_Id" +
+                        ") " +
+                        "VALUES(" +
+                        "   ?," +
+                        "   ?," +
+                        "   ?" +
+                        ")"
+                );
+
+                stmt.setString(1, enter);
+                stmt.setString(2, "Enter");
+                stmt.setString(3, questionNaireId);
+
+                stmt.executeUpdate();
+
+                stmt.close();
+
+            }
+
+            String questionId;
+            String[] questionOptions;
+
+            for (Pair<String, String[]> single : question_Single) {
+
+                stmt = conn.prepareStatement("" +
+                        "INSERT INTO question(" +
+                        "   question_Cont, " +
+                        "   question_Kind, " +
+                        "   questionnaire_Id" +
+                        ") " +
+                        "VALUES(" +
+                        "   ?," +
+                        "   ?," +
+                        "   ?" +
+                        ")"
+                );
+
+                stmt.setString(1, single.getKey());
+                stmt.setString(2, "Single");
+                stmt.setString(3, questionNaireId);
+
+                stmt.executeUpdate();
+
+                stmt.close();
+
+                stmt = conn.prepareStatement("SELECT LAST_INSERT_ID()");
+
+                rs = stmt.executeQuery();
+
+                rs.next();
+
+                questionId = rs.getString(1);
+
+                rs.close();
+
+                stmt.close();
+
+                questionOptions = single.getValue();
+
+                for (String option : questionOptions) {
+
+                    stmt = conn.prepareStatement("" +
+                            "INSERT INTO questionoption(" +
+                            "   question_Id, " +
+                            "   option_Cont" +
+                            ") " +
+                            "VALUES(" +
+                            "   ?," +
+                            "   ?" +
+                            ")"
+                    );
+
+                    stmt.setString(1, questionId);
+                    stmt.setString(2, option);
+
+                    stmt.executeUpdate();
+
+                    stmt.close();
+
+                }
+
+            }
+
+            for (Pair<String, String[]> multiple : question_Multiple) {
+
+                stmt = conn.prepareStatement("" +
+                        "INSERT INTO question(" +
+                        "   question_Cont, " +
+                        "   question_Kind, " +
+                        "   questionnaire_Id" +
+                        ") " +
+                        "VALUES(" +
+                        "   ?," +
+                        "   ?," +
+                        "   ?" +
+                        ")"
+                );
+
+                stmt.setString(1, multiple.getKey());
+                stmt.setString(2, "Multiple");
+                stmt.setString(3, questionNaireId);
+
+                stmt.executeUpdate();
+
+                stmt.close();
+
+                stmt = conn.prepareStatement("SELECT LAST_INSERT_ID()");
+
+                rs = stmt.executeQuery();
+
+                rs.next();
+
+                questionId = rs.getString(1);
+
+                rs.close();
+
+                stmt.close();
+
+                questionOptions = multiple.getValue();
+
+                for (String option : questionOptions) {
+
+                    stmt = conn.prepareStatement("" +
+                            "INSERT INTO questionoption(" +
+                            "   question_Id, " +
+                            "   option_Cont" +
+                            ") " +
+                            "VALUES(" +
+                            "   ?," +
+                            "   ?" +
+                            ")"
+                    );
+
+                    stmt.setString(1, questionId);
+                    stmt.setString(2, option);
+
+                    stmt.executeUpdate();
+
+                    stmt.close();
+
+                }
+
+            }
+
+            conn.close();
 
             response.sendRedirect("./Servlet_LookThroughQuestionnaires");
 
